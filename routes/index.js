5869bf165dedc5e1183fdd633d748e9bdbbc0ff8
Ron31 <ron3108@gmx.net>
2020-05-03 21:08:07 +0200

    Fixing Issues

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
-var snekfetch = require('snekfetch')
+var snekfetch = require("snekfetch");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', async function (req, res) {
+router.get("/", async function (req, res) {
   if(req.query.date) {
     let answer = await snekfetch.get("http://localhost:5000/date/" + req.query.date);
-    return res.render('index', {active: 'index', color: answer.body.color, action: answer.body.Nachricht});
+    return res.render("index", {active: "index", color: answer.body.color, action: answer.body.Nachricht});
   } else {
-    res.render('index', {active: 'index'});
+    res.render("index", {active: "index"});
   }
 });
 
